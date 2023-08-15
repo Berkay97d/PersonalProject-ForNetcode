1baea323d347fd4fe4ddc3c45daed1540c35b73d
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-08-15 17:46:19 +0300

    Network UI & Player Initilization On Connect

---
+++ Assets/Scripts/NetworkUI.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkUI : MonoBehaviour
+{
+    [SerializeField] private Button serverButton;
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button clientButton;
+
+
+    private void Start()
+    {
+        serverButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+        
+        hostButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        
+        clientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
